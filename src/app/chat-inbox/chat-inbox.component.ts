393371184745b9a87e88fde640d2773a599df203
Rafik Al Shmoury <49872889+rafiksh@users.noreply.github.com>
2021-03-19 16:47:06 +0200

    handled chat socket

--- src/app/chat-inbox/chat-inbox.component.ts
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { io } from 'socket.io-client';
+
 import { SOCKET_ENDPOINT } from 'src/config/config.env';
 
 @Component({
@@ -9,6 +10,7 @@ import { SOCKET_ENDPOINT } from 'src/config/config.env';
 })
 export class ChatInboxComponent implements OnInit {
   socket: any;
+  message: any;
 
   constructor() {}
 
@@ -18,5 +20,27 @@ export class ChatInboxComponent implements OnInit {
 
   setupSocketConnection() {
     this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('chat', (data: any) => {
+      console.log(data.message);
+      if (data.message) {
+        const element = document.createElement('li');
+        element.innerHTML = data.message;
+        element.style.background = 'white';
+        element.style.padding = '15px 30px';
+        element.style.margin = '10px';
+        document.getElementById('message-list')?.appendChild(element);
+      }
+    });
+  }
+  SendMessage() {
+    this.socket.emit('chat', { message: this.message, handle: 'MR' });
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding = '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    document.getElementById('message-list')?.appendChild(element);
+    this.message = '';
   }
 }
